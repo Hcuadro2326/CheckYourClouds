ce63b00a5a50992442e60dd21ab91cda54c7ae86
Hector <cuadro.bebo2326@gmail.com>
2022-11-10 00:46:58 -0600

    final

--- weather.js
+++ weather.js
@@ -1,34 +1,18 @@
-// const url = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={818a74417384dcdcda67ec49319e6bd4}';
-const APIKey = '818a74417384dcdcda67ec49319e6bd4';
+const APIkey = "818a74417384dcdcda67ec49319e6bd4";
 
-
-
-// const getIt = function() {
-// const weatherURL = 'https://api.openweathermap.org'
-//     fetch(weatherURL).then(function(response){
-//       response.json().then(function(data){
-//             console.log(data)
-//         });
-//     });
-// };
-// console.log("hello");
-
-
-// getIt();
-
-
-const nameEl = document.getElementById('nameEl');
-const currentTime = document.getElementById('currentTime');
-
+$(document).on("click", "#search-button", () => {
+    let x = $("#search-input").val();
+    //console.log(x);
+    currentWeather(x);
+});
 
 function currentWeather(search) {
-    //let url = "https://api.openweathermap.org/data/2.5/weather?q=" + search + "&appid=" + APIKey;
-    let url = `https://api.openweathermap.org/data/2.5/forecast?appid=${APIKey}&q=${search}`
+    let url = `https://api.openweathermap.org/data/2.5/forecast?appid=${APIKey}&q=${search}`;
 
-    console.log(url);
+    //console.log(url);
     fetch(url)
         .then(function (response) {
-            if(response.ok){
+            if (response.ok) {
                 return response.json();
             } else {
                 alert("broken");
@@ -36,76 +20,73 @@ function currentWeather(search) {
         })
         .then(function (data) {
             // let weatherData = data
-            console.log(data);
-            nameEl.innerHTML = data.city.name;
-            currentTime.innerHTML = new Date();
-            let weatherIcon = data.weather[0].icon;
-            console.log(weatherIcon);
-            let focusImg = currentIcon.setAttribute("src", "https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png");
-            currentTemp.innerHTML = "Temperature: " + Math.round(((data.main.temp - 273.15) * 1.8) + 32);
-            currentWind.innerHTML = "Wind Speed: " + data.wind.speed + "MPH";
-            currentHumidity.innerHTML = "Humidity: " + data.main.humidity;
+            //console.log(data);
+            document.getElementById("currentCity").innerHTML = data.city.name;
+            document.getElementById("currentTime").innerHTML = new Date();
+            //console.log(data.list[0].weather[0].icon);
+            let icon = data.list[0].weather[0].icon;
+            let focusImg = document.getElementById("currentIcon").setAttribute("src", `http://openweathermap.org/img/wn/${icon}@2x.png`);
+            document.getElementById("currentTemp").innerHTML = "Temperature: " + Math.round((data.list[0].main.temp - 273.15) * 1.8 + 32);
+            document.getElementById("currentWind").innerHTML = "Wind Speed: " + data.list[0].wind.speed + "MPH";
+            document.getElementById("currentHumidity").innerHTML = "Humidity: " + data.list[0].main.humidity;
 
-            let lat = data.coord.lat;
-            let lon = data.coord.lon;
+            let lat = data.city.coord.lat;
+            let lon = data.city.coord.lon;
             let UVRequestURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey + "&cnt=1";
 
-// Nested function to find UV Index and change background color based on that value
+            // Nested function to find UV Index and change background color based on that value
             fetch(UVRequestURL)
                 .then(function (response) {
-                    return response.json();
+                    if (response.ok) {
+                        return response.json();
+                    } else {
+                        alert("broken");
+                    }
                 })
                 .then(function (data) {
-                    console.log(data);
-                    let UVIndex = document.createElement("span")
+                    //console.log(data);
+                    let UVIndex = document.createElement("span");
 
-                    if (data[0].value < 4 ) {
+                    if (data[0].value < 4) {
                         UVIndex.setAttribute("class", "badge badge-success");
-                    }
-                    else if (data[0].value < 8) {
+                    } else if (data[0].value < 8) {
                         UVIndex.setAttribute("class", "badge badge-warning");
-                    }
-                    else {
+                    } else {
                         UVIndex.setAttribute("class", "badge badge-danger");
                     }
-                    UVIndex.innerHTML= (data[0].value);
-                    currentUV.innerHTML = "UV Index: ";
-                    currentUV.appendChild(UVIndex);
-                })
-// Nested function to pull 5 day data
-                let cityID = data.id;
-                let forecastRequestURL = "https://api.openweathermap.org/data/2.5/forecast?id=" + cityID + "&appid=" + APIKey;
-                fetch(forecastRequestURL)
+                    UVIndex.innerHTML = data[0].value;
+                    document.getElementById("currentUV").innerHTML = "UV Index: ";
+                    document.getElementById("currentUV").appendChild(UVIndex);
+                });
+            // Nested function to pull 5 day data
+            let cityID = data.city.id;
+            //console.log(cityID);
+            let forecastRequestURL = "https://api.openweathermap.org/data/2.5/forecast?id=" + cityID + "&appid=" + APIKey;
+            fetch(forecastRequestURL)
                 .then(function (response) {
                     return response.json();
                 })
                 .then(function (data) {
-                    console.log(data);
-                    const forecastEl = document.querySelectorAll("#fiveday")
-                    for (i=0; i < forecastEl.length; i++){
-                        forecastEl[i].innerHTML="";
+                    //console.log(data);
+                    const forecastEl = document.querySelectorAll(".fiveday");
+                    for (i = 0; i < forecastEl.length; i++) {
+                        forecastEl[i].innerHTML = "";
                         let forecastIndex = i * 8 + 4;
 
                         let forecastDate = document.createElement("p");
                         forecastDate.innerHTML = data.list[forecastIndex].dt_txt;
-                        forecastEl[i].appendChild(forecastDate)
+                        forecastEl[i].appendChild(forecastDate);
                         let forecastIcon = document.createElement("img");
                         forecastIcon.setAttribute("src", "https://openweathermap.org/img/wn/" + data.list[forecastIndex].weather[0].icon + "@2x.png");
                         forecastIcon.setAttribute("alt", data.list[forecastIndex].weather[0].description);
                         forecastEl[i].append(forecastIcon);
                         let forecastTemp = document.createElement("p");
-                        forecastTemp.innerHTML = "Temperature: " + Math.round(((data.list[forecastIndex].main.temp - 273.15) * 1.8) + 32);
+                        forecastTemp.innerHTML = "Temperature: " + Math.round((data.list[forecastIndex].main.temp - 273.15) * 1.8 + 32);
                         forecastEl[i].appendChild(forecastTemp);
                         let forecastHumidity = document.createElement("p");
                         forecastHumidity.innerHTML = "Humidity: " + data.list[forecastIndex].main.humidity;
                         forecastEl[i].appendChild(forecastHumidity);
                     }
-                })
+                });
         });
-
 }
-
-$(document).on('click', '#search-button', () => {
-    let x = $('#search-input').val();
-    currentWeather(x);
-})
